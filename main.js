5e7dc503bf9a561e683410f4cda75a09d438dabc
yehor <ivashin.egor98@gmail.com>
2023-02-24 12:50:33 +0200

    deploy 3.0

--- main.js
+++ main.js
@@ -30,13 +30,3 @@ function calcRation() {
   }
   messageAboutActivity.classList.remove('hide');
 }
-
-function totalRation(params, person, ration) {
-  return (
-    (params[0] +
-      params[1] * person.weight +
-      params[2] * person.height -
-      params[3] * person.age) *
-    ration
-  ).toFixed(1);
-}
